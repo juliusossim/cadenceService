2b99a2d0bd02e8ea9b0579e22ac4318e407106d4
Julius Ossim <juliusjacksonossim@gmail.com>
2021-03-01 17:50:06 +0100

    cong

--- config/database.php
+++ config/database.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Str;
 
 $url = env("CLEARDB_DATABASE_URL");
-$host = $url["host"] ? $url["host"]: null;
+$host = $url["host"] ?? null;
 //$host = 'us-cdbr-east-03.cleardb.com';
-$username = $url["user"] ?  $url["user"]: null;
+$username = $url["user"] ?? null;
 //$username = 'bd3a2e949366fb';
-$password = $url["pass"] ?$url["pass"]: null;
+$password = $url["pass"] ?? null;
 //$password = 'dec99831';
-$database = substr($url["path"], 1) ?substr($url["path"], 1): null;
+$database = substr($url["path"], 1) ?? null;;
 //$database = 'heroku_42b139cd9f5bce0';
 
 return [
