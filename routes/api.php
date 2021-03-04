7767d8e78f095a204975d6cbbb0aba5e4f5ce566
Julius Ossim <juliusjacksonossim@gmail.com>
2021-03-04 09:54:19 +0100

    put -> patch

--- routes/api.php
+++ routes/api.php
@@ -24,19 +24,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('sequence', 'App\Http\Controllers\SequenceController@index');
 Route::get('sequence/{sequence}', 'App\Http\Controllers\SequenceController@show');
 Route::post('sequence', 'App\Http\Controllers\SequenceController@store');
-Route::put('sequence/{sequence}', 'App\Http\Controllers\SequenceController@update');
+Route::patch('sequence/{sequence}', 'App\Http\Controllers\SequenceController@update');
 Route::delete('sequence/{sequence}', 'App\Http\Controllers\SequenceController@delete');
 
 /*emailSequence*/
 Route::get('emailSequence', 'App\Http\Controllers\EmailSequenceController@index');
 Route::get('emailSequence/{emailSequence}', 'App\Http\Controllers\EmailSequenceController@show');
 Route::post('emailSequence', 'App\Http\Controllers\EmailSequenceController@store');
-Route::put('emailSequence/{emailSequence}', 'App\Http\Controllers\emailSequenceController@update');
+Route::patch('emailSequence/{emailSequence}', 'App\Http\Controllers\emailSequenceController@update');
 Route::delete('emailSequence/{emailSequence}', 'EmailSequenceController@delete');
 
 /*taskSequence*/
 Route::get('taskSequence', 'App\Http\Controllers\TaskSequenceController@index');
 Route::get('taskSequence/{taskSequence}', 'App\Http\Controllers\TaskSequenceController@show');
 Route::post('taskSequence', 'App\Http\Controllers\TaskSequenceController@store');
-Route::put('taskSequence/{taskSequence}', 'App\Http\Controllers\TaskSequenceController@update');
+Route::patch('taskSequence/{taskSequence}', 'App\Http\Controllers\TaskSequenceController@update');
 Route::delete('taskSequence/{taskSequence}', 'App\Http\Controllers\TaskSequenceController@delete');
